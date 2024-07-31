5a5404a30fe84645f36ffd8b9ded6d6cf7e3ea2f
Corey Hz <153377078+HeKtonk@users.noreply.github.com>
2024-07-31 15:11:27 +0200

    V2 du projet
    Résolution de problème lié aux nombre flotant
    Piece 2 centimes implémenté
    Optimisation du code (moins de répétition de code)
    Prise en charge en cas de monnaie donnée insuffisante

--- Js/app.js
+++ Js/app.js
@@ -1,116 +1,94 @@
-// Gérer le rendu de monnaie
-function encaissement(){
+// Déclaration des variables globales
+let monnaie = 0;
+let montantTotal = 0;
+let listeMontantDonneEntier = [];
+let prixArticleInitial = 0;
+const nomMonnaie = ["nbBillet500", "nbBillet200", "nbBillet100", "nbBillet50", "nbBillet20", "nbBillet10", "nbBillet5", "nbPiece2", "nbPiece1", "nbPiece05", "nbPiece02", "nbPiece01", "nbPiece005", "nbPiece002", "nbPiece001"];
+let monnaieARendre = [];
+// Valeur en centimes
+const valeurMonnaie = [50000, 20000, 10000, 5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1];
 
+// Gérer le rendu de monnaie
+function encaissement(monnaie){
+
+    // Je récupère les données des inputs et les convertis en nombre flotant
     let montantDonne = parseFloat(document.getElementById("montantClient").value);
     let montantDu = parseFloat(document.getElementById("prixArticle").value);
 
-    const isValid = isNumber(montantDonne, montantDu);
+    // Je garde en mémoire le montant dû initial
+    if(prixArticleInitial === 0){
+        prixArticleInitial = montantDu;
+    }
 
-    if (!isValid){
+    // Je teste si mes deux variables sont bien du type nombre et pas NaN
+    if (!isNumber(montantDonne, montantDu)){
         return document.getElementById("conteneurOutput").innerHTML = "Veuillez entrer des données valides !";
     }
 
-    const monnaieRendue = {};
-
     // Convertir les nombres à virgule en nombres entiers
-    montantDu = montantDu * 100;
-    montantDonne = montantDonne * 100;
+    montantDonne = convertionMonnaie(montantDonne);
+    montantDu = convertionMonnaie(montantDu);
 
-    let monnaie = montantDonne - montantDu;
+    // Je garde en mémoire les montants donnés une fois convertis
+    listeMontantDonneEntier.push(montantDonne);
 
-    // Rendre la monnaie
-    let nbBillet500 = monnaie / 50000;
-    monnaie = monnaie % 50000;
-    if (nbBillet500 >= 1){
-        monnaieRendue["Billet(s) de 500€"] = Math.floor(nbBillet500);
+    // Si le montant n'est pas suffisant je demande le reste
+    if(montantDonne < montantDu){
+        monnaie = montantDu - montantDonne;
+        if(monnaie.countDecimals() > 2){
+            monnaie = Math.round(monnaie);
+        }
+        let newValue = monnaie.toString().slice(0, (monnaie.toString().length - 2)) + "." + monnaie.toString().slice(monnaie.toString().length - 2);
+        document.getElementById("conteneurOutput").innerHTML = `Monnaie insuffisante, il vous manque ${newValue}€ s'il vous plaît`;
+        document.getElementById("prixArticle").value = newValue;
+        document.getElementById("prixArticle").disabled = true;
+        return null;
     }
 
-    let nbBillet200 = monnaie / 20000;
-    monnaie = monnaie % 20000;
-    if (nbBillet200 >= 1){
-        monnaieRendue["Billet(s) de 200€"] = Math.floor(nbBillet200);
+    // Je remet les montants corrects pour effectuer le rendu de monnaie
+    montantDonne = 0;
+    for (const montant of listeMontantDonneEntier) {
+        montantTotal += montant;
     }
+    montantDu = convertionMonnaie(prixArticleInitial);
 
-    let nbBillet100 = monnaie / 10000;
-    monnaie = monnaie % 10000;
-    if (nbBillet100 >= 1){
-        monnaieRendue["Billet(s) de 100€"] = Math.floor(nbBillet100);
-    }
+    // Je récupère le montant à rendre
+    monnaie = montantTotal - montantDu;
 
-    let nbBillet50 = monnaie / 5000;
-    monnaie = monnaie % 5000;
-    if (nbBillet50 >= 1){
-        monnaieRendue["Billet(s) de 50€"] = Math.floor(nbBillet50);
-    }
+    // Je compte la monnaie
+    monnaie = comptageMonnaie(nomMonnaie, monnaie);
 
-    let nbBillet20 = monnaie / 2000;
-    monnaie = monnaie % 2000;
-    if (nbBillet20 >= 1){
-        monnaieRendue["Billet(s) de 20€"] = Math.floor(nbBillet20);
-    }
+    // On affiche la monnaie
+    affichageMonnaie(monnaieARendre, monnaie);
 
-    let nbBillet10 = monnaie / 1000;
-    monnaie = monnaie % 1000;
-    if (nbBillet10 >= 1){
-        monnaieRendue["Billet(s) de 10€"] = Math.floor(nbBillet10);
-    }
-
-    let nbBillet5 = monnaie / 500;
-    monnaie = monnaie % 500;
-    if (nbBillet5 >= 1){
-        monnaieRendue["Billet(s) de 5€"] = Math.floor(nbBillet5);
-    }
-
-    let nbPiece2 = monnaie / 200;
-    monnaie = monnaie % 200;
-    if (nbPiece2 >= 1){
-        monnaieRendue["Piece(s) de 2€"] = Math.floor(nbPiece2);
-    }
-
-    let nbPiece1 = monnaie / 100;
-    monnaie = monnaie % 100;
-    if (nbPiece1 >= 1){
-        monnaieRendue["Piece(s) de 1€"] = Math.floor(nbPiece1);
-    }
-
-    let nbPiece05 = monnaie / 50;
-    monnaie = monnaie % 50;
-    if (nbPiece05 >= 1){
-        monnaieRendue["Piece(s) de 0.50€"] = Math.floor(nbPiece05);
-    }
-
-    let nbPiece02 = monnaie / 20;
-    monnaie = monnaie % 20;
-    if (nbPiece02 >= 1){
-        monnaieRendue["Piece(s) de 0.20€"] = Math.floor(nbPiece02);
-    }
-
-    let nbPiece01 = monnaie / 10;
-    monnaie = monnaie % 10;
-    if (nbPiece01 >= 1){
-        monnaieRendue["Piece(s) de 0.10€"] = Math.floor(nbPiece01);
-    }
-
-    let nbPiece005 = monnaie / 5
-    monnaie = monnaie % 5;
-    if (nbPiece005 >= 1){
-        monnaieRendue["Piece(s) de 0.05€"] = Math.floor(nbPiece005);
-    }
-
-    let nbPiece001 = monnaie / 1
-    if (nbPiece001 >= 1){
-        monnaie = monnaie % 1;
-        monnaieRendue["Piece(s) de 0.01€"] = Math.floor(nbPiece001);
-    }
-
-        affichageMonnaie(monnaieRendue, monnaie);
-
-    // On retourne l'objet monnaieRendue qui est un type de tableau
-    return monnaieRendue;
-
+    // On retourne le tableau monnaieRendue qui contient le nombre de chaque monnaie
+    return monnaieARendre;
 }
 
-// Tester si c'est bien un nombre
+// Convertir la monnaie
+function convertionMonnaie(montant) {
+    if((montant + 0.01) !== Math.round(montant)){
+        montant = (montant * 100);
+    }
+    if((montant + 0.01) === Math.round(montant)){
+        montant = (((montant + 0.01) * 100)-1);
+    }
+    // On retourne le tableau monnaieRendue qui contient le nombre de chaque monnaie
+    return montant;
+}
+
+// Compter la monnaie
+function comptageMonnaie(nomMonnaie, monnaie) {
+    for (let i = 0; i <= 14; i++){
+        let nbMonnaie = 0;
+        nbMonnaie = monnaie / valeurMonnaie[i];
+        monnaie = monnaie % valeurMonnaie[i];
+        monnaieARendre.push(Math.floor(nbMonnaie));
+    }
+    return monnaie;
+}
+
+// Tester si c'est bien un nombre valide
 function isNumber(montantDonne, montantDu){
     let isValid = false;
     if(typeof montantDonne === "number" && !Number.isNaN(montantDonne) && typeof montantDu === "number" && !Number.isNaN(montantDu)){
@@ -122,13 +100,20 @@ function isNumber(montantDonne, montantDu){
 // Mettre à jour la page HTML
 function affichageMonnaie(monnaieRendue, monnaieRestant) {
 
+    document.getElementById("monnaie").disabled = true;
+    document.getElementById("montantClient").value = montantTotal.toString().slice(0, (montantTotal.toString().length - 2)) + "." + montantTotal.toString().slice(montantTotal.toString().length - 2);
+    document.getElementById("prixArticle").value = prixArticleInitial;
+
     if (monnaieRestant !== 0){
-        return document.getElementById("conteneurOutput").innerHTML = "Une erreur s'est produite, veuillez essayer à nouveau !";
+        return document.getElementById("conteneurOutput").innerHTML = `Une erreur s'est produite, veuillez essayer à nouveau ! <br> Rendre les ${prixArticleInitial}€`;
     }
 
     let text = "";
-    for (let [monnaie, value] of Object.entries(monnaieRendue)) {
-        text += monnaie + ": " + value + "<br>";
+
+    for (let i = 0; i <= monnaieRendue.length; i++){
+        if(monnaieRendue[i] >= 1){
+            text += nomMonnaie[i] + " : " + monnaieRendue[i] + "<br>";
+        }
     }
     return document.getElementById("conteneurOutput").innerHTML = text;
 }
@@ -138,4 +123,18 @@ function reset(){
     document.getElementById("montantClient").value = 0;
     document.getElementById("prixArticle").value = 0;
     document.getElementById("conteneurOutput").innerHTML = "";
+    document.getElementById("prixArticle").disabled = false;
+    document.getElementById("monnaie").disabled = false;
+    // Je réinitialise mes variables globales
+    monnaie = 0;
+    montantTotal = 0;
+    prixArticleInitial = 0;
+    listeMontantDonneEntier.length = 0;
+    monnaieARendre.length = 0;
+}
+
+// compte le nombre de décimale d'un nombre flotant
+Number.prototype.countDecimals = function () {
+    if(Math.floor(this.valueOf()) === this.valueOf()) return 0;
+    return this.toString().split(".")[1].length || 0;
 }
